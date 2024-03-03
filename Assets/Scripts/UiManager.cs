7a0795421690b595033fd6d745a0dfe0a8150504
Simona <sima.simute1999@gmail.com>
2024-03-03 11:44:17 +0200

    Score

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
@@ -25,9 +26,66 @@ public class UiManager : MonoBehaviour
         finalScoreText.text = score.ToString();
         
         scoreText.gameObject.SetActive(false);
+
+        int best = 0, second = 0;
+        if (PlayerPrefs.HasKey("best"))
+        {
+            best = PlayerPrefs.GetInt("best");
+
+            if (score > best)
+            {
+                best = score;
+                PlayerPrefs.SetInt("best", best);
+                PlayerPrefs.Save();
+            }
+
+            bestScoreText.text = best.ToString();
+        }
+        else
+        {
+            bestScoreText.text = score.ToString();
+            PlayerPrefs.SetInt("best", score);
+
+        }
+
+        if (PlayerPrefs.HasKey("second"))
+        {
+            second = PlayerPrefs.GetInt("second");
+            
+            if (score > second && score < best)
+            {
+                second = score;
+                PlayerPrefs.SetInt("second", second);
+               
+            }
+        }
+        else
+        {
+            second = score;
+        }
         
-        //TODO: Implement best score
-        bestScoreText.text = "9999";
-        medalImage.sprite = medals[Random.Range(0, medals.Length)];
+        PlayerPrefs.Save();
+         
+        //medal according to score
+        if (score >= best)
+        {
+            medalImage.sprite = medals[0];
+        }
+        else if (score >= second)
+        {
+            medalImage.sprite = medals[1];
+        }
+        else
+        {
+            medalImage.sprite = medals[2];
+        }
+        
+        
+        //medalImage.sprite = medals[Random.Range(0, medals.Length)];
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
